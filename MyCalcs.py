6321d7aaeaeb708731f21da4401fcd76d645a1e2
Lincoln de Souza oliveira <lincoln.so.18@hotmail.com>
2024-09-06 19:10:22 -0300

    Foi criado o algoritmo de calculos e operadores

---
+++ MyCalcs.py
@@ -0,0 +1,15 @@
+#Demonstração do  uso de operadores
+x = int(input("Digite um numero: "))
+y = int(input("Digite outro numero: "))
+
+SOMA = x + y 
+print(f"O resultado da soma é: {SOMA}")
+
+SUBTRACAO = x - y
+print(f"O resultado da subtração é: {SUBTRACAO}")
+
+MULTIPLICACAO = x * y 
+print(f"O resultado da multiplicaçao é: {MULTIPLICACAO}")
+
+DIVISAO = x / y
+print(f"O resultado da divisao é: {DIVISAO}")
---
+++ MyOperator.py
@@ -0,0 +1,14 @@
+#Demonstração de expressão complexas
+
+#Declaração de variaveis que iremos usar..
+
+x = 10; y = 20; z = 30
+
+VALOR = x * (y / 10 + z / 10) - 25
+print(f"O resultado da expressao #1 é: {VALOR}")
+
+VALOR = ((y - x) + (z - x)) / 3
+print(f"O resultado da expressão #2 é: {VALOR}")
+
+VALOR = (x + (y * 2 +(z / 10))) - 18
+print(f"O resultado da expressão #3 é {VALOR}")
