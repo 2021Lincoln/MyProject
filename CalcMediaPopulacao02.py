9e31258c25bccab632a273b63d42dad13c2479be
@LincolnOliveira <86691883+2021Lincoln@users.noreply.github.com>
2024-09-16 21:41:22 -0300

    Add files via upload

---
+++ CalcMediaPopulacao02.py
@@ -0,0 +1,39 @@
+"""
+A prefeitura de uma cidade deseja fazer uma pesquisa entre seus habitantes. 
+Faça um programa para coletar dados sobre a renda familiare numero de filhos de cada habitantate e ao final, mostre:
+
+a) Média de salario da população
+b) Média de numeros de filhos
+c) Maior salario dos habitantes 
+d) percentual de pessoas com salario menor que R$1500,00.
+
+"""
+soma_renda = 0
+soma_qtd_filhos = 0
+maior_renda = 0
+qtd_renda = 0
+cont = 0
+
+resp = "S"
+while resp == "S" or resp == "s":
+
+    #Coletando os dados
+    renda = float(input("Digite a renda familiar: "))
+    qtd_filhos = int(input("Digite o numero de filhos: "))
+    #Acumulando os dados
+    soma_renda += + renda
+    soma_qtd_filhos += qtd_filhos
+    #Coletando a maior renda
+    if renda > maior_renda:
+        maior_renda = renda
+    #Coletando renda menor que 1500
+    if renda < 1500:
+        qtd_renda += 1
+    cont += 1 #Acumulando a quantidade de pessoas
+    resp = input("Deseja continuar: (S/N)?")
+#Final do bloco de repetição
+#Exibindo os resultado
+print(f"A media das rendas: {soma_renda/cont}")
+print(f"A media das quantidade de filhos: {soma_qtd_filhos/cont}")
+print(f"A Maior renda é: {maior_renda}")
+print(f"O percentual de pessoas com renda abaixo de 1500 é: {(qtd_renda/cont)*100}")
